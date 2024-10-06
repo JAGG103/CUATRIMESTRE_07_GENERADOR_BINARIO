3597b678f4c1997c8ab784c4b49e7c27682a7de1
JAGG103 <roma_jhonatan_alexander@outlook.com>
2024-10-06 00:00:24 -0600

    Se terminó la función de evaluación para cuantificadores universal y existencial asi como funciones auxiliares

--- MODULES/GENERATOR/EvaluationAlgorithm.py
+++ MODULES/GENERATOR/EvaluationAlgorithm.py
@@ -7,14 +7,12 @@ class EvaluationAlgorithm:
         self.init_ = self.main(evaluations, init)
         
 
-    def main(self,evaluations:dict, init:dict):
-        
+    def main(self,evaluations:dict, init:dict)->dict:
         for variable in init.keys():
             globals()[variable] = init[variable]
 
         functions = [self.evaluate_universal, self.evaluate_existential]
         names = ['universal evaluation','existential evaluation']
-
         for i in range(len(names)):
             for predicate in evaluations[names[i]]:
                 functions[i](predicate, init.keys())
@@ -23,88 +21,33 @@ class EvaluationAlgorithm:
             init[variable] = eval(variable)
         return init
     
+
     def evaluate_existential(self, predicate:str, globvars:list):
+        # Este operador realizara los efectos siempre que la causa se cumpla, el numero de veces que la implicación se cumpla
         FORALL,EXISTS = (1,2)
-        AND, OR, NONE = (1,2,3)
-        DNUM, DELEM, DINDS = (1,2,3)
-        SET,REL = [0,1]
         existential = Quantifiers(predicate,EXISTS)
-        inset = Operator('set').inset_
-        notin = Operator('set').notin_
-
-        for atom in existential.atoms:
-            op = Operator('logic')
-            atom = atom[1:-1]
-            inds = get_indexes(op.implies_, atom)
-            causes = atom[:inds[0][0]]
-            efect = atom[inds[0][1]:]
-
-            inds_and = get_indexes(op.and_, causes)
-            inds_or = get_indexes(op.or_, causes)
-            if(inds_and):
-                subatoms, operator = get_elements_notin_indexes(inds_and, causes), AND
-            elif(inds_or):
-                subatoms, operator = get_elements_notin_indexes(inds_or, causes), OR
-            else:
-                subatoms, operator = [causes], NONE
-        
-            start = eval(existential.start)
-            end = eval(existential.end.replace("\\", "\\\\"))
-            errors = list()
-            for i in range(start, end):
-                errors_ = list()
-                element = str(i) if existential.domtype in {DNUM, DINDS} else f"{existential.domain}[{i}]"
-                for atom in subatoms:
-                    atom = replace_pattern(existential.iterv, element, atom)
-                    option = SET if get_indexes(inset+'|'+notin, atom) else REL
-                    if(option == REL):
-                        op = Operator('relational')
-                        pattern = rf"{op.less_}|{op.greater_}|{op.equality_}"
-                    elif(option == SET):
-                        op = Operator('set')
-                        pattern = rf"{op.inset_}|{op.notin_}"
-                    inds = get_indexes(pattern, atom)
-                    operator_ = atom[inds[0][0]:inds[0][1]]
-                    left, right = split_with_pattern(pattern, atom)
-                    error_ = Evaluate().relational(left, right, operator_) if option==REL else Evaluate().set(left, right, operator_)
-                    errors_ += [error_] 
-                errors.append(sum(errors_) if operator in {AND, NONE} else min(errors_))
-            #error = min(errors)
-            #if(error == 0.0):
-            executions = errors.count(0.0)
-            for _ in range(executions):
-                equality = Operator('relational').equality_
-                left, right = split_with_pattern(equality, efect)
-                for globvar in globvars:
-                    modvar = rf"{globvar}(?!\[)"
-                    modele = rf"{globvar}(?=\[)"
-                    inds_modvar = get_indexes(modvar, left)
-                    inds_modele = get_indexes(modele, left)
-                    if(inds_modvar):
-                        globals[globvar] = eval(right)
-                    elif(inds_modele):
-                        pinds = r"(?<=\[)\d+(?=\])"
-                        inds = get_indexes(pinds, left)
-                        position = left[inds[0][0]:inds[0][1]]
-                        globals()[globvar][int(position)] = eval(right)
-
+        self.auxiliary_quantifiers(existential, globvars, EXISTS)
+            
     def evaluate_universal(self, predicate:str, globvars:list):
+        # Este operador realizara el efecto una vez si se cumple el efecto.
         FORALL,EXISTS = (1,2)
+        universal = Quantifiers(predicate,FORALL)
+        self.auxiliary_quantifiers(universal, globvars, FORALL)
+
+
+    def auxiliary_quantifiers(self, quantifier:Quantifiers, globvars:list, OPTION:int):
         AND, OR, NONE = (1,2,3)
         DNUM, DELEM, DINDS = (1,2,3)
         SET,REL = [0,1]
-        universal = Quantifiers(predicate,FORALL)
-
         inset = Operator('set').inset_
         notin = Operator('set').notin_
 
-        for atom in universal.atoms:
+        for atom in quantifier.atoms:
             op = Operator('logic')
             atom = atom[1:-1]
             inds = get_indexes(op.implies_, atom)
             causes = atom[:inds[0][0]]
             efect = atom[inds[0][1]:]
-
             inds_and = get_indexes(op.and_, causes)
             inds_or = get_indexes(op.or_, causes)
             if(inds_and):
@@ -113,15 +56,14 @@ class EvaluationAlgorithm:
                 subatoms, operator = get_elements_notin_indexes(inds_or, causes), OR
             else:
                 subatoms, operator = [causes], NONE
-        
-            start = eval(universal.start)
-            end = eval(universal.end.replace("\\", "\\\\"))
+            start = eval(quantifier.start)
+            end = eval(quantifier.end.replace("\\", "\\\\"))
             errors = list()
             for i in range(start, end):
                 errors_ = list()
-                element = str(i) if universal.domtype in {DNUM, DINDS} else f"{universal.domain}[{i}]"
+                element = str(i) if quantifier.domtype in {DNUM, DINDS} else f"{quantifier.domain}[{i}]"
                 for atom in subatoms:
-                    atom = replace_pattern(universal.iterv, element, atom)
+                    atom = replace_pattern(quantifier.iterv, element, atom)
                     option = SET if get_indexes(inset+'|'+notin, atom) else REL
                     if(option == REL):
                         op = Operator('relational')
@@ -135,26 +77,30 @@ class EvaluationAlgorithm:
                     error_ = Evaluate().relational(left, right, operator_) if option==REL else Evaluate().set(left, right, operator_)
                     errors_ += [error_] 
                 errors.append(sum(errors_) if operator in {AND, NONE} else min(errors_))
-            
-            #error = sum(errors) if universal.operator in {AND, NONE} else max(errors)
-            #if(error == 0.0):
+            self.auxiliary_evaluations(efect, globvars, errors, OPTION)
+
+
+    def auxiliary_evaluations(self, efect:str, globvars:list, errors:list, OPTION:int):
+        FORALL,EXISTS = (1,2)
+        if(OPTION == FORALL):
             executions = errors.count(0.0)
-            for _ in range(executions):
-                equality = Operator('relational').equality_
-                left, right = split_with_pattern(equality, efect)
-                for globvar in globvars:
-                    modvar = rf"{globvar}(?!\[)"
-                    modele = rf"{globvar}(?=\[)"
-                    inds_modvar = get_indexes(modvar, left)
-                    inds_modele = get_indexes(modele, left)
-                    if(inds_modvar):
-                        globals[globvar] = eval(right)
-                    elif(inds_modele):
-                        pinds = r"(?<=\[)\d+(?=\])"
-                        inds = get_indexes(pinds, left)
-                        position = left[inds[0][0]:inds[0][1]]
-                        globals()[globvar][int(position)] = eval(right)
+        elif(OPTION == EXISTS):
+            executions = 1 if min(errors)==0.0 else 0
+        else:
+            raise ValueError(f"Opción invalida: {OPTION}")
+        for _ in range(executions):
+            equality = Operator('relational').equality_
+            left, right = split_with_pattern(equality, efect)
+            for globvar in globvars:
+                modvar = rf"{globvar}(?!\[)"
+                modele = rf"{globvar}(?=\[)"
+                inds_modvar = get_indexes(modvar, left)
+                inds_modele = get_indexes(modele, left)
+                if(inds_modvar):
+                    globals[globvar] = eval(right)
+                elif(inds_modele):
+                    pinds = r"(?<=\[)\d+(?=\])"
+                    inds = get_indexes(pinds, left)
+                    position = left[inds[0][0]:inds[0][1]]
+                    globals()[globvar][int(position)] = eval(right)
 
-
-    def auxiliary_evaluation(self):
-        pass
