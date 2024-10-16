ddf97bd5f14c60f4a39287cafe8ca880dae5a4de
JAGG103 <roma_jhonatan_alexander@outlook.com>
2024-10-16 09:42:39 -0600

    Se creo un nuevo archivo para los patrones que en principio se llama 'regex_patterns2' y se estan actualizando los modulos del generador para funcionar con este, al igual que la correccion de bugs con los patrones, hasta ahora se han migrado 3 de los 4 modulos, faltando el generador

--- MODULES/ANALYZER/Analyzer.py
+++ MODULES/ANALYZER/Analyzer.py
@@ -1,18 +1,19 @@
-from MODULES.regex_patterns import Blocks, Specification, Operator, Delimiters
 from MODULES.regex_functions import get_indexes_blocks, split_with_pattern, get_indexes, indexes_avoiding_head_and_tail, get_elements_notin_indexes, splitp_lookbehind_and_lookhead
 
+from MODULES.regex_patterns2 import Block, Operator, Specification, Quantifier, Delimiter
+
 class Analyzer:
-    __slots__ = ('init', 'inPort','inAuxPort','outPort','outAuxPort','prels', 'testConditions','defConditions')
+    __slots__ = ('init', 'inport','inaux','outport','outaux','prels', 'testconditions','defconditions')
 
     def __init__(self, specification:str):
         self.init = {}
-        self.inPort = {}
-        self.inAuxPort = {}
-        self.outPort = {}
-        self.outAuxPort = {}
+        self.inport = {}
+        self.inaux = {}
+        self.outport = {}
+        self.outaux = {}
         self.prels = []
-        self.testConditions = []
-        self.defConditions = []
+        self.testconditions = []
+        self.defconditions = []
         
         processInit, processUT = self.get_processes(specification)
         self.get_init(processInit)
@@ -21,49 +22,57 @@ class Analyzer:
 
     # GET_PROCESSES
     def get_processes(self, specification):
-        indexes = get_indexes_blocks(specification, Blocks('process')._process, Blocks('process').process_)
+        block = Block()
+        indexes = get_indexes_blocks(specification, block.process, block.endprocess)
         processInit = specification[indexes[0][0]:indexes[0][1]]
         processUT = specification[indexes[1][0]:indexes[1][1]]
         return processInit, processUT
     
     # GET_INIT
     def get_init(self, processInit:str):
+        operator = Operator()
         init = {}
         lines = processInit.split('\n')
         lines.pop(0)
         lines.pop(-1)
         if(lines!=[]):
             for line in lines:
-                clave, valor = split_with_pattern(Operator('relational').equality_, line)
+                clave, valor = split_with_pattern(operator.equality, line)
                 valor = eval(valor)
                 init.update({clave:valor})
         self.init = init
 
     # GET_INFO_PROCESSUT
     def get_info_processUT(self, processUT:str)->list:
-        linesls = processUT.split('\n')
+        specification = Specification()
+        inds = get_indexes(specification.post, processUT)
+        prepost = processUT[:inds[0][0]]
+        postpost = processUT[inds[0][0]:]
+        linesls = prepost.split('\n')
+        inds = get_indexes(specification.end_process, postpost)
+        linesls += [postpost[:inds[0][0]].replace('\n','')]
         for line in linesls:
-            if(len(line)==0 or get_indexes(Specification('keys').end_process,line)):
+            if(len(line)==0):
                 continue
-            elif(get_indexes(Specification('keys').process, line)!=None):
-                self.inPort, self.outPort = self.get_ports(line)
-            elif(get_indexes(Specification('keys').aux,line)!=None):
-                self.inAuxPort, self.outAuxPort = self.get_ports(line)
-            elif(get_indexes(Specification('keys').ext, line)!=None):
+            elif(get_indexes(specification.process, line)!=None):
+                self.inport, self.outport = self.get_ports(line)
+            elif(get_indexes(specification.aux,line)!=None):
+                self.inaux, self.outaux = self.get_ports(line)
+            elif(get_indexes(specification.ext, line)!=None):
                 self.get_external_variables(line)
-            elif(get_indexes(Specification('keys').pre, line)!=None):
+            elif(get_indexes(specification.pre, line)!=None):
                 self.get_pre(line)
-            elif(get_indexes(Specification('keys').post, line)!=None):
+            elif(get_indexes(specification.post, line)!=None):
                 postls = self.return_post(line)
             else:
                 raise ValueError(f"Elemento en la especificación que no corresponde {line}")
         return postls
     
-    # GET_TESTCONDITIONS_DEFCONDITIONS
+    # GET_testconditions_defconditions
     def get_testconditions_defconditions(self, prels, postls):
         tcs,dcs = [],[]
-        patterns = [rf"\b{variable}\b" for variable in self.outPort['variables']]
-        patterns_= [rf"\b{variable}\b" for variable in self.outAuxPort['variables']]  
+        patterns = [rf"\b{variable}\b" for variable in self.outport['variables']]
+        patterns_= [rf"\b{variable}\b" for variable in self.outaux['variables']]  
         patterns = r"|".join(patterns + patterns_)
         fss = self.return_functional_scenarios(prels, postls)
         for fs in fss:
@@ -75,8 +84,8 @@ class Analyzer:
                     tc.append(atom)
             tcs += [tc]
             dcs += [dc]
-        self.testConditions = tcs
-        self.defConditions = dcs
+        self.testconditions = tcs
+        self.defconditions = dcs
         
         
     
@@ -88,13 +97,13 @@ class Analyzer:
         return fss
 
     def get_pre(self, line:str):
-        quan = Blocks('quantifiers')
-        logic = Operator('logic')
-        specification = Specification('keys')
+        quantifier = Quantifier()
+        operator = Operator()
+        specification = Specification()
 
         inds = get_indexes(specification.pre, line)
         line = line[inds[0][1]:]
-        inds = indexes_avoiding_head_and_tail([quan._forall,quan._exists],[quan.forall_,quan.exists_],logic.and_,line)
+        inds = indexes_avoiding_head_and_tail([quantifier.forall, quantifier.exists],[quantifier.endquant, quantifier.endquant], operator.and_, line)
         if(inds):
             self.prels = get_elements_notin_indexes(inds, line)
         elif(line != ''):
@@ -103,11 +112,15 @@ class Analyzer:
         
             
     def return_post(self, line:str):
-        inds = get_indexes(Specification('keys').post, line)
+        specification = Specification()
+        delimiter = Delimiter()
+        operator = Operator()
+        quantifier = Quantifier()
+        inds = get_indexes(specification.post, line)
         poststr = line[inds[0][1]:]
-        postls = splitp_lookbehind_and_lookhead(Delimiters('disyuntos').disyuntos_, Operator('logic').or_, Delimiters('disyuntos')._disyuntos, poststr)
+        postls = splitp_lookbehind_and_lookhead(delimiter.enddisyuntos, operator.or_, delimiter.disyuntos, poststr)
         postls = [e[1:-1] for e in postls]
-        indexesgen = (indexes_avoiding_head_and_tail([Blocks('quantifiers')._forall, Blocks('quantifiers')._exists],[Blocks('quantifiers').forall_, Blocks('quantifiers').exists_], Operator('logic').and_, element) for element in postls)
+        indexesgen = (indexes_avoiding_head_and_tail([quantifier.forall, quantifier.exists],[quantifier.endquant, quantifier.endquant], operator.and_, element) for element in postls)
         for i in range(len(postls)):
             indexes = next(indexesgen)
             postls[i] = get_elements_notin_indexes(indexes, postls[i])
@@ -115,7 +128,7 @@ class Analyzer:
 
 
     def get_ports(self, line:str)->tuple[dict]:
-        specification = Specification('ports')
+        specification = Specification()
         ip = get_indexes(specification.inport, line)
         op = get_indexes(specification.outport, line)
         if(ip and op):
@@ -136,11 +149,12 @@ class Analyzer:
         
 
     def turn_ports_to_dict(self, port:str|None)->dict:
+        specification = Specification()
         variables, types = [],[]
         if(port):
-            portls = port.split(Specification('ports').sepv)
+            portls = port.split(specification.separator)
             for element in portls:
-                variable, typee = element.split(Specification('ports').sept)
+                variable, typee = element.split(specification.instdom)
                 variables += [variable]
                 types += [typee]
         port = {'variables':variables, 'types':types}
@@ -148,27 +162,26 @@ class Analyzer:
 
 
     def get_external_variables(self, line:str):
-        specification = Specification('keys')
-        ext = specification.ext
-        specification = Specification('ports')
+        specification = Specification()
+
         inport = {'variables':[], 'types':[]}
         outport = {'variables':[], 'types':[]}
-        indexes = get_indexes(ext, line)
+        indexes = get_indexes(specification.ext, line)
         extstr = line[indexes[0][1]:]
         if(extstr!=''):
-            extls = extstr.split(specification.sepv)
+            extls = extstr.split(specification.separator)
             for element in extls:
                 indexesrd = get_indexes(specification.rd, element)
                 indexeswr = get_indexes(specification.wr, element)
                 if(indexesrd!=None):
                     info = element[indexesrd[0][1]:]
-                    variable,typee = info.split(specification.sept)
+                    variable,typee = info.split(specification.separator)
                     variable = '_'+variable
                     inport['variables'].append(variable)
                     inport['types'].append(typee)
                 elif(indexeswr!=None):
                     info = element[indexeswr[0][1]:]
-                    variable,typee = info.split(specification.sept)
+                    variable,typee = info.split(specification.separator)
                     outport['variables'].append(variable)
                     outport['types'].append(typee)
                     variable = '_'+variable
@@ -176,7 +189,7 @@ class Analyzer:
                     inport['types'].append(typee)
                 else:
                     raise ValueError("Entrada no valida wr o rd")
-        self.inPort['variables'] += inport['variables']
-        self.inPort['types'] += inport['types']
-        self.outPort['variables'] += outport['variables']
-        self.outPort['types'] += outport['types']
+        self.inport['variables'] += inport['variables']
+        self.inport['types'] += inport['types']
+        self.outport['variables'] += outport['variables']
+        self.outport['types'] += outport['types']
--- MODULES/MUTATOR/Mutator.py
+++ MODULES/MUTATOR/Mutator.py
@@ -1,29 +1,32 @@
 from MODULES.regex_functions import indexes_avoiding_head_and_tail
-from MODULES.regex_patterns import Operator, Delimiters, Mutations
+
+from MODULES.regex_patterns2 import Operator, Quantifier, Mutation
+
+import copy
 
 class Mutator:
-    __slots__ = ('precondition','testConditions','defConditions')
-    def __init__(self, precondition:list, testConditions:list[list], defConditions:list[list]):
+    __slots__ = ('precondition','testconditions','defconditions')
+    def __init__(self, precondition:list, testconditions:list[list], defconditions:list[list]):
         self.precondition = precondition
-        self.testConditions = testConditions
-        self.defConditions = defConditions
+        self.testconditions = testconditions
+        self.defconditions = defconditions
         self.main()
         
 
     def main(self):
         # Se extienden las condiciones de prueba manteniendo sus condiciones de definición originales
-        #adjust = (2*len(self.precondition))-len(self.precondition)
+        #adjust = (2*len(self.precondition))-len(self.precondition) # Si queremos que no mute las precondiciones decomentar esta linea y comntar la siguiente
         adjust = 0
-        self.extend_testconditions_defconditions(adjust, self.testConditions, self.defConditions)
+        self.extend_testconditions_defconditions(adjust, self.testconditions, self.defconditions)
         # Se extienden las condiciones de definición manteniendo sus condiciones de prueba originales
         adjust = 0
-        self.extend_testconditions_defconditions(adjust, self.defConditions, self.testConditions)
+        self.extend_testconditions_defconditions(adjust, self.defconditions, self.testconditions)
 
 
     def extend_testconditions_defconditions(self, adjust:int, targetConditions:list[list], fixedConditions:list[list])->None:
-        relational = Operator('relational')
-        delimiter = Delimiters('evaluation')
-        mutation = Mutations()
+        operator = Operator()
+        quantifier = Quantifier()
+        mutation = Mutation()
 
         condition = True
         while(condition):
@@ -32,35 +35,38 @@ class Mutator:
                 if(condition):
                     break
                 for i in range(adjust, len(targetConditions[it])):
-                    extDict = {}
-                    indexes_le = indexes_avoiding_head_and_tail([delimiter._evaluation],[delimiter.evaluation_], relational.le_, targetConditions[it][i])
-                    indexes_ge = indexes_avoiding_head_and_tail([delimiter._evaluation],[delimiter.evaluation_], relational.ge_, targetConditions[it][i])
-                    indexes_ne = indexes_avoiding_head_and_tail([delimiter._evaluation],[delimiter.evaluation_], relational.inequality_, targetConditions[it][i])
+                    mutants = {}
+                    indexes_le = indexes_avoiding_head_and_tail([quantifier.forall, quantifier.exists],[quantifier.endquant, quantifier.endquant], operator.le, targetConditions[it][i])
+                    indexes_ge = indexes_avoiding_head_and_tail([quantifier.forall, quantifier.exists],[quantifier.endquant, quantifier.endquant], operator.ge, targetConditions[it][i])
+                    indexes_ne = indexes_avoiding_head_and_tail([quantifier.forall, quantifier.exists],[quantifier.endquant, quantifier.endquant], operator.inequality, targetConditions[it][i])
                     if(indexes_le):
-                        extDict = self.extend_set_of_predicates(targetConditions[it], fixedConditions[it], i, indexes_le, mutation.less, mutation.equal)
+                        mutants = self.extend_set_of_predicates(targetConditions[it],fixedConditions[it], i, indexes_le, mutation.less, mutation.equal)
                     elif(indexes_ge):
-                        extDict = self.extend_set_of_predicates(targetConditions[it], fixedConditions[it], i, indexes_ge, mutation.greater, mutation.equal)
+                        mutants = self.extend_set_of_predicates(targetConditions[it],fixedConditions[it], i, indexes_ge, mutation.greater, mutation.equal)
                     elif(indexes_ne):
-                        extDict = self.extend_set_of_predicates(targetConditions[it], fixedConditions[it], i, indexes_ne, mutation.greater, mutation.less)
+                        mutants = self.extend_set_of_predicates(targetConditions[it],fixedConditions[it], i, indexes_ne, mutation.greater, mutation.less)
                     else:
                         pass
 
-                    if(extDict!={}):
+                    if(mutants!={}):
                         targetConditions.pop(it)
                         fixedConditions.pop(it)
-                        targetConditions.insert(it, extDict['operationA'][0])
-                        fixedConditions.insert(it, extDict['operationA'][1])
-                        targetConditions.insert(it, extDict['operationB'][0])
-                        fixedConditions.insert(it, extDict['operationB'][1])
+                        targetConditions.insert(it, mutants['mutant A'][0])
+                        fixedConditions.insert(it, mutants['mutant A'][1])
+                        targetConditions.insert(it, mutants['mutant B'][0])
+                        fixedConditions.insert(it, mutants['mutant B'][1])
                         condition = True
                         break
         
-    def extend_set_of_predicates(self, obj:list, piv: list, i: int, indexes:str, extOpA:str, extOpB:str)->tuple[list, list]:
-        ext_objA = obj.copy()
-        elem = obj[i][:indexes[0][0]] + extOpA + obj[i][indexes[0][1]:]
-        ext_objA[i] = elem
-        ext_objB = obj.copy()
-        elem = obj[i][:indexes[0][0]] + extOpB + obj[i][indexes[0][1]:]
-        ext_objB[i] = elem
-        output = {'operationA':(ext_objA, piv.copy()), 'operationB':(ext_objB, piv.copy())}
-        return output
+    def extend_set_of_predicates(self, obj:list, piv:list, i:int, indexes:list[tuple], operatorA:str, operatorB:str)->dict[str,tuple]:
+        mutantA = copy.deepcopy(obj)
+        mutantB = copy.deepcopy(obj)
+
+        elem = obj[i][:indexes[0][0]] + operatorA + obj[i][indexes[0][1]:]
+        mutantA[i] = elem
+
+        elem = obj[i][:indexes[0][0]] + operatorB + obj[i][indexes[0][1]:]
+        mutantB[i] = elem
+
+        mutants = {'mutant A':(mutantA,piv), 'mutant B':(mutantB,piv)}
+        return mutants
